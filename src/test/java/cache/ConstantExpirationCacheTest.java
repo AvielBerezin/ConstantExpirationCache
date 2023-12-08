52b58566e51237541ebecb4913dd1d177728c1e7
aviel.berezin <aviel.berezin@gmail.com>
2023-12-08 11:17:29 +0200

    removed unused test and renamed a test

--- src/test/java/cache/ConstantExpirationCacheTest.java
+++ src/test/java/cache/ConstantExpirationCacheTest.java
@@ -1,5 +1,23 @@
 package cache;
 
-public class ConstantExpirationCacheTest {
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+public class ConstantExpirationCacheTest {
+    @Test
+    public void onExpiration() throws InterruptedException {
+        ConstantExpirationCache<Integer, String> cache = new ConstantExpirationCache<>(Duration.ofMillis(500), x -> {
+            System.out.printf("%s %s%n", Instant.now(), x);
+        });
+        cache.put(1, "hello");
+        Thread.sleep(200);
+        cache.put(2, "world");
+        Thread.sleep(1000);
+        Assertions.assertThat(cache.values(Collectors.reducing((ignored1, ignored2) -> ignored1)))
+                  .isEmpty();
+    }
 }
